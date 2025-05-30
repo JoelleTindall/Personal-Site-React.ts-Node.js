137ea2a30a965c567a34cb32cc219f11bf17bdb8
JoelleTindall <jtindall222@gmail.com>
2025-05-30 17:51:06 -0400

    5/30

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-     
-const contactRoutes = require('./routes/contact');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { Client } = require("pg");
+const contactRoutes = require("./routes/contact");
 
 const app = express();
-const PORT = 8000;
+ const PORT = process.env.PORT;
+
+//db
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_KEY,
+  port: process.env.DB_PORT,
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL database!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
 
 // Middleware
 app.use(cors());
@@ -13,9 +31,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api', contactRoutes);
+app.use("/api", contactRoutes);
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
